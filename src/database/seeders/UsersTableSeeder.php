34c2520cc1815b73a1bf64b84923dd6490dbe7b6
Crabvn <huynhtoan.com@gmail.com>
2024-09-26 14:51:15 +0800

    Connect database and seed admin

--- src/database/seeders/UsersTableSeeder.php
+++ src/database/seeders/UsersTableSeeder.php
@@ -4,16 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
-     *
-     * @return void
      */
-    public function run()
+    public function run(): void
     {
-        $this->call(UsersTableSeeder::class);
+        User::factory()->create();
     }
 }
